ab8692b1b7d962fa28a585e64cac8b8033594e7f
kasri.elmehdi@gmail.com <kasri.elmehdi@gmail.com>
2024-02-01 21:20:25 +0000

    COMMIT

--- models/int_orders_operational.sql
+++ models/int_orders_operational.sql
@@ -2,6 +2,7 @@ select
     orders_id,
     date_date,
     margin,
+    revenue,
     round ((margin + shipping_fee - logcost - ship_cost), 2) AS operational_margin
 from {{ ref("int_orders_margin") }}
 join {{ ref("stg_raw__ship") }} using (orders_id)
--- models/mart/finance_days.sql
+++ models/mart/finance_days.sql
@@ -1,8 +1,16 @@
-select
-    date_date,
-    round(sum(revenue),2) as total_revenue,
-    round(sum(margin),2) as total_margin,
-    count(distinct orders_id) as nb_transactions,
-    round(sum(revenue) / count(distinct orders_id), 2) as average_basket
-from {{ ref('int_orders_margin') }}
-group by date_date
+SELECT
+    date_date
+    ,COUNT(orders_id) AS nb_transactions
+    ,ROUND(SUM(revenue),0) AS revenue 
+    ,ROUND(AVG(revenue),1) AS average_basket
+    ,ROUND(SUM(revenue)/COUNT(orders_id),1) AS average_basket_bis
+    ,ROUND(SUM(margin),0) AS margin 
+    ,ROUND(SUM(operational_margin),0) AS operational_margin 
+    ,ROUND(SUM(purchase_cost),0) AS purchase_cost 
+    ,ROUND(SUM(shipping_fee),0) AS shipping_fee 
+    ,ROUND(SUM(logcost),0) AS logcost 
+    ,ROUND(SUM(ship_cost),0) AS ship_cost 
+    ,SUM(quantity) AS quantity 
+FROM {{ref("int_orders_operational")}}
+GROUP BY  date_date
+ORDER BY  date_date DESC
