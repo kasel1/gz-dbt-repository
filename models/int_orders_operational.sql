eccffcbc8a564022d8b746da996ead7ccdc48ae8
kasri.elmehdi@gmail.com <kasri.elmehdi@gmail.com>
2024-02-01 21:14:02 +0000

    commit

--- models/int_orders_operational.sql
+++ models/int_orders_operational.sql
@@ -1,6 +1,7 @@
 select
     orders_id,
     date_date,
+    margin,
     round ((margin + shipping_fee - logcost - ship_cost), 2) AS operational_margin
 from {{ ref("int_orders_margin") }}
 join {{ ref("stg_raw__ship") }} using (orders_id)
